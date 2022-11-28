6ee9caa0e3e92b0f1467705e3a3db98ae0c0fd8a
Aboueim <aboueim@mcmaster.ca>
2022-11-27 22:35:54 -0500

    Earthquakes_past7days files Added

---
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -0,0 +1,44 @@
+// Add console.log to check to see if our code is working.
+console.log("Working")
+
+// We create the street view tile layer that will be the default background of our map.
+let streets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v12",
+  accessToken: API_KEY
+});
+
+// We create the street view tile layer that will be an option for our map.
+let satelliteStreets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/satellite-streets-v12",
+  accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  'Streets': streets,
+  'Satellite': satelliteStreets
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('map', {
+  center: [39.5, -98.5],
+  zoom: 3,
+  layers: [streets]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
+});
